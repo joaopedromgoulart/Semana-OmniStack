21c891a6a79594ca1ff2d8e15046be2da855a2ea
joao Pedro M Goulart <joaopedromgoulart@gmail.com>
2019-08-09 18:44:13 -0300

    Aula 3 ate 01:11:00, problemas com a requisicao do dislike

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,6 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './main.css';
+
+import api from '../services/api';
+
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
 
 export default function Main({ match }) {
-    console.log('FODASEEEEEEE')
-    return <h1>{match.params.id}</h1>
+    const [users, setUsers] = useState([]);
+
+    useEffect(() =>{
+        async function loadUsers () {
+            const response = await api.get('/devs',{
+                headers: {
+                    user: match.params.id,
+                 }
+            });
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [match.params.id]);
+
+    async function handleLike(id) {
+        console.log('like', id);
+    }
+
+    async function handleDislike(id) {
+        await api.post(`/dev/${id}/dislikes`, null, {
+            headers: { user : match.params.id },
+        })
+    }
+
+    return (
+    <div className="main-container">
+        <img src={logo} alt="Tindev"></img>
+        <ul>
+            {users.map(user => (
+                <li key={user._id}>
+                    <img src={user.avatar} alt={user.name} />
+                    <footer>
+                        <strong>{user.name}</strong>
+                        <p>{user.bio}</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button" onClick={() => handleDislike(user._id)}>
+                        <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button" onClick={() => handleLike(user._id)}>
+                        <img src={like} alt="like" />
+                        </button>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    </div>
+    );
 }
